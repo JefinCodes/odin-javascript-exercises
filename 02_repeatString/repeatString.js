6711e94ae3eec52bb65cf2ad2e987ffa2da188e9
jefin <techy.jefinjoji@gmail.com>
2025-03-30 08:25:45 +0530

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if(times < 0){
+        return "ERROR";
+    }
+    let text = "";
+    while(times > 0){
+        text+= word;
+        times--;
+    }
+    return text;
 };
 
 // Do not edit below this line
