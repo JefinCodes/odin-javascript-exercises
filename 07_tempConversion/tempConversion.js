818727558c7d295bce29b29972f4f82fc4a96456
jefin <techy.jefinjoji@gmail.com>
2025-03-31 12:20:55 +0530

    exercise 7 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+function roundTo1Decimal(num){
+  num = num * 10;
+  num = Math.round(num);
+  num = num / 10;
+  return num;
+}
+
+const convertToCelsius = function(tempFah) {
+  let tempCel = (tempFah - 32) * 5 / 9;
+  if(Number.isInteger(tempCel)){
+    return tempCel;
+  }
+  return roundTo1Decimal(tempCel);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+  let tempFah = tempCel * 9 / 5 + 32;
+  if(Number.isInteger(tempFah)){
+    return tempFah;
+  }
+  return roundTo1Decimal(tempFah);
 };
 
 // Do not edit below this line
