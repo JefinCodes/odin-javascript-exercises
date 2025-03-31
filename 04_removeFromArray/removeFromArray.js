1c01c3366f0f5a53199e7faff837a65329d3c555
jefin <techy.jefinjoji@gmail.com>
2025-03-31 11:12:14 +0530

    exercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let modifiedArr = arr;
+    for(let i = 1; i < arguments.length; i++){
+        modifiedArr = modifiedArr.filter(item => item !== arguments[i])
+    }
+    const resultArr = modifiedArr;
+    return resultArr;
 };
 
 // Do not edit below this line
