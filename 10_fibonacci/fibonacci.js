38c8b8db69977175be87ed22eefe4d05eac34e36
jefin <techy.jefinjoji@gmail.com>
2025-06-14 09:11:42 +0530

    exercise 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const cleanedNum = parseInt(num);
+    if(cleanedNum < 0){
+        return "OOPS";
+    }
+    if(cleanedNum == 0){
+        return 0;
+    }
+    let fib1 = 0;
+    let fib2 = 1;
+    for(let i = 1; i < cleanedNum; i++){
+        let temp = fib2;
+        fib2 = fib1 + fib2;
+        fib1 = temp;
+    }
+    return fib2;
 };
 
 // Do not edit below this line
