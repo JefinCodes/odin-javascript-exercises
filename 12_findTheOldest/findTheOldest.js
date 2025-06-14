501827402110477c4b5ebc94e18fc4bbfe62e0ea
jefin <techy.jefinjoji@gmail.com>
2025-06-14 14:17:04 +0530

    exercise 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people){
+    const currentYear = new Date().getFullYear();
+    return people.reduce((oldest, currentElement) => {
+        const deathYear = currentElement.yearOfDeath || currentYear;
+        const age = deathYear - currentElement.yearOfBirth;
+        const oldestDeathYear = oldest.yearOfDeath || currentYear;
+        const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+        return (age > oldestAge)? currentElement : oldest;
+    }, people[0]);
 };
 
 // Do not edit below this line
